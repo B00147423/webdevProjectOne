4232d33ae8732b1f393037eaa1661f66d41598a8
B00147423 <b00147423@mytudublin.ie>
2023-11-26 17:17:25 +0000

    4th new url string added

--- src/app/api/getProducts/route.js
+++ src/app/api/getProducts/route.js
@@ -4,7 +4,7 @@ export async function GET(req, res) {
     console.log("in the api page")
     // =================================================
     const { MongoClient } = require('mongodb');
-    const url = 'mongodb+srv://betsunaidzeb:Pls45StopThis45@cluster0.jdtjs2r.mongodb.net/app?retryWrites=true&w=majority';
+    const url = 'mongodb+srv://betsunaidzeb:Pls45StopThis45@?@cluster0.vgnfxgh.mongodb.net/?retryWrites=true&w=majority';
     const client = new MongoClient(url);
     const dbName = 'app'; // database name
     await client.connect();
--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -13,7 +13,7 @@ export async function GET(req, res) {
   console.log(dob);
   // =================================================
   const { MongoClient } = require('mongodb');
-  const url = 'mongodb+srv://betsunaidzeb:Pls45StopThis45@cluster0.jdtjs2r.mongodb.net/app?retryWrites=true&w=majority';
+  const url = 'mongodb+srv://betsunaidzeb:Pls45StopThis45@?@cluster0.vgnfxgh.mongodb.net/?retryWrites=true&w=majority';
   const client = new MongoClient(url);
   const dbName = 'app'; // database name
   await client.connect();
